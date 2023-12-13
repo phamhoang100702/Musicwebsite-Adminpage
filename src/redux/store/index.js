229e5ef8bfcb8366e418cc9bd2b8e7dd4001ec39
Phạm Hoàng <phamhoang100702@gmail.com>
2023-12-13 17:57:42 +0700

    s

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { allReducers } from "../reducers";
-import { thunk } from "redux-thunk";
 
 export const store = createStore(allReducers);
